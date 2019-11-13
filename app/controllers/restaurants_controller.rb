2e9bc0c512abbce1cdb930542679686ec1e5b9bd
Sibylle Eloy <sibylleeloy@hotmail.com>
2019-11-13 13:03:38 +0100

    restaurants index view

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,18 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+end
